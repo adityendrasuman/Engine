77f7799616a81a10544b1f2fee96c8fc0c67a836
adityendrasuman <34791829+adityendrasuman@users.noreply.github.com>
2021-07-07 12:25:13 +0800

    Add files via upload

--- Engine/create_new_columns_in_r.R
+++ Engine/create_new_columns_in_r.R
@@ -4,26 +4,28 @@
   # create a new column:    mutate
   # if else condition:      case_when
   # (in)equality:           ==, >, <, <=, >=, %in%, |, &, is.na(), !
-  # 
 
-# SAMPLE:
-  # df_out <- df_in %>% 
+create_new_col <- function(df_in){
+  
+  df_out <- df_in
+  # ########################################
+  # FORMULA FOR NEW COLUMNS IN THIS SECTION:
+  
+  # SAMPLE:
+  # df_out <- df_out %>% 
   #   mutate(new_var = case_when(
   #     old_var_1 %in% c("A", "B") ~ "Value 1",       Meaning: when old_var_1 is EITHER A OR B then new_var = Value 1
   #     !(old_var_2 %in% c("C", "D")) ~ "Value 2",    Meaning: else when old_var_2 is NEITHER C NOR D then new_var = value 2
   #     T                          ~ "value 3"        Meaning: else new_var = "Value 3"
   # ))
-
-# DOING CHECKS AND GETTING DATA:
-if (!exists("d_01_D") | !is.data.frame(d_01_D)){}
-
-create_new_col <- function(df_in){
   
-  # ########################################
-  # FORMULA FOR NEW COLUMNS IN THIS SECTION:
+  # C01: -----
+  df_out <- df_out %>% 
+    mutate(state_new = "BIHAR")
   
-  
-  
+  # C02: -----
+  df_out <- df_out %>% 
+    mutate(state_new_2 = "Uttar Pradesh")
   
   
   # ########################################
